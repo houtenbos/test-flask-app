8db29cfe62c122c894e8b4018cf486e2686f4ba8
Lester Houtenbos <ljphoutenbos@gmail.com>
2018-06-29 15:38:33 +0200

    update 9000

--- app.py
+++ app.py
@@ -11,31 +11,34 @@ collection = db.test_collection
 
 @app.route('/')
 def hello_world():
-    entries = collection.find()
+    try:
+        entries = collection.find()
 
-    msg = ''
-    date = ''
-    posts = []
+        msg = ''
+        date = ''
+        posts = []
 
-    for entry in entries:
-        if 'msg' in entry:
-            msg = entry['msg']
-        if 'date' in entry:
-            date = entry['date']
-        post = msg + '<br>' + str(date)
-        posts.append(post)
+        for entry in entries:
+            if 'msg' in entry:
+                msg = entry['msg']
+            if 'date' in entry:
+                date = entry['date']
+            post = msg + '<br>' + str(date)
+            posts.append(post)
 
-    head = '<html>'
-    end = '</html>'
-    content = ''
-    for post in posts:
-        content += '<br>' + post + '<br>'
-    page = head + content + end
-    return page
+        head = '<html>'
+        end = '</html>'
+        content = ''
+        for post in posts:
+            content += '<br>' + post + '<br>'
+        page = head + content + end
+        return page
+    except Exception as e:
+        print('Error:')
+        print(e)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 80))
+    port = int(os.environ.get('PORT', 5000))
     print(port)
-    port = 80
     app.run(host='127.0.0.1', port=port)
 
